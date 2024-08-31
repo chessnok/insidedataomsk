eb39de4d4f0ac335b9b9efbbd7c1bb4a9e5c0b45
Mikhail Fadin <fadin.edu@gmail.com>
2024-08-31 17:19:26 +0200

    STart core app

--- insidedataomsk/settings.py
+++ insidedataomsk/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*1c(%e+!ot86&jx+*(e-&$6e!#p^bw9f2^qiuvbnd7n7$ihoop"
+SECRET_KEY = "django-insecure-*1c(%e+!ot86&jx+*(e-&$6e!#p^bw9f2^qiuvbnd7n7$ihop"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core.apps.CoreConfig"
 ]
 
 MIDDLEWARE = [
